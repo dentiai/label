07c960a64f7d56e0489d8eb2a54a2731d3b4c766
Mohammad Ashour <ashour.mohammad@gmail.com>
2017-03-14 17:44:34 +0200

    Refactor: Renamed “components” to “containers” for convention.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
-import App from './components/App';
+import App from './containers/App';
 import './index.css';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
